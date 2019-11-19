60985f358fbc1bee80cf228a525f0e899a2d9a1f
ryandoherty1 <37849271+ryandoherty1@users.noreply.github.com>
2019-11-19 14:13:00 -0800

    Add files via upload

--- functions.js
+++ functions.js
@@ -37,8 +37,8 @@ var topbox = document.getElementsByClassName("top-box");
 var bottombox = document.getElementsByClassName("bottom-box");
 var clicksound = new Audio('audio/click.mp3');
 var kahoot = new Audio('audio/kahoot.mp3');
-var viewportWidth = window.innerWidth;
-var viewportHeight = window.innerHeight;
+var viewportWidth = document.documentElement.clientWidth;
+var viewportHeight = document.documentElement.clientHeight;
 var random;
 var clock;
 var movement;
@@ -135,19 +135,566 @@ function gameOver() {
     }
   b1.style.backgroundColor = "#F0CC8D";
   }
-function generateRandom() {
-  random = button[Math.floor(Math.random() *button.length)];
-  console.log(random);
-}
-function generateRandom2() {
-    random = button[Math.floor(Math.random() * 16)];
-    console.log(random);
-}
 function randomMove() {
-  if (random == button[0]) {
-    if (check == 0) {
-      generateRandom();
-      randomMove();
+  if (viewportWidth < 776 ) {
+    if (random == button[0]) {
+      if (check == 0) {
+        generateRandom2();
+        randomMove();
+    } else {
+      for (i = 0; i < button.length; i++) {
+        button[i].style.display = "none";
+	    button[0].style.display = "block";
+      }
+      check = 0;
+	}
+  } else if (random == button[1]) {
+      if (check == 1) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+        button[i].style.display = "none";
+        button[1].style.display = "block";
+	    }
+      check = 1;
+      }
+	} else if (random == button[2]) {
+      if (check == 2) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+        button[i].style.display = "none";
+        button[2].style.display = "block";
+	    }
+      check = 2;
+      }
+	} else if (random == button[3]) {
+      if (check == 3) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[3].style.display = "block";
+	    }
+      check = 3;
+      }
+	} else if (random == button[4]) {
+      if (check == 4) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[4].style.display = "block";
+	    }
+      check = 4;
+      }
+	} else if (random == button[5]) {
+      if (check == 5) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[5].style.display = "block";
+	    }
+      check = 5;
+      }
+	} else if (random == button[6]) {
+          if (check == 6) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[6].style.display = "block";
+	    }
+      check = 6;
+      }
+	} else if (random == button[7]) {
+      if (check == 7) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[7].style.display = "block";
+	    }
+      check = 7;
+      }
+	} else if (random == button[8]) {
+      if (check == 8) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[8].style.display = "block";
+	    }
+      check = 8;
+      }
+	} else if (random == button[9]) {
+      if (check == 9) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[9].style.display = "block";
+	    }
+      check = 9;
+      }
+	} else if (random == button[10]) {
+      if (check == 10) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[10].style.display = "block";
+	    }
+      check = 10;
+      }
+	} else if (random == button[11]) {
+          if (check == 11) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[11].style.display = "block";
+	    }
+      check = 11;
+      }
+	} else if (random == button[12]) {
+      if (check == 12) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[12].style.display = "block";
+	    }
+      check = 12;
+      }
+	} else if (random == button[13]) {
+      if (check == 13) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[13].style.display = "block";
+	    }
+      check = 13;
+      }
+	} else if (random == button[14]) {
+      if (check == 14) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[14].style.display = "block";
+	    }
+      check = 14;
+      }
+	} else if (random == button[15]) {
+      if (check == 15) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[15].style.display = "block";
+	    }
+      check = 15;
+      }
+	} else if (random == button[16]) {
+      if (check == 16) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[16].style.display = "block";
+	    }
+      check = 16;
+      }
+	} else if (random == button[17]) {
+      if (check == 17) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[17].style.display = "block";
+	    }
+        check = 17;
+      }
+	} else if (random == button[18]) {
+      if (check == 18) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[18].style.display = "block";
+	    }
+        check = 18;
+      }
+	} else if (random == button[19]) {
+      if (check == 19) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[19].style.display = "block";
+	    }
+        check = 19;
+      }
+	} else if (random == button[20]) {
+      if (check == 20) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[20].style.display = "block";
+	    }
+        check = 20;
+      }
+	} else if (random == button[21]) {
+      if (check == 21) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[21].style.display = "block";
+	    }
+      check = 21;
+      }
+	} else if (random == button[22]) {
+      if (check == 22) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[22].style.display = "block";
+	    }
+      check = 22;
+      }
+	} else if (random == button[23]) {
+      if (check == 23) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[23].style.display = "block";
+	    }
+      check = 2;
+      }
+	} else if (random == button[24]) {
+      if (check == 24) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[23].style.display = "block";
+	    }
+      check = 24;
+      }
+	}
+  } else if (viewportHeight < 801) {
+        if (random == button[0]) {
+      if (check == 0) {
+        generateRandom2();
+        randomMove();
+    } else {
+      for (i = 0; i < button.length; i++) {
+        button[i].style.display = "none";
+	    button[0].style.display = "block";
+      }
+      check = 0;
+	}
+  } else if (random == button[1]) {
+      if (check == 1) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+        button[i].style.display = "none";
+        button[1].style.display = "block";
+	    }
+      check = 1;
+      }
+	} else if (random == button[2]) {
+      if (check == 2) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+        button[i].style.display = "none";
+        button[2].style.display = "block";
+	    }
+      check = 2;
+      }
+	} else if (random == button[3]) {
+      if (check == 3) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[3].style.display = "block";
+	    }
+      check = 3;
+      }
+	} else if (random == button[4]) {
+      if (check == 4) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[4].style.display = "block";
+	    }
+      check = 4;
+      }
+	} else if (random == button[5]) {
+      if (check == 5) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[5].style.display = "block";
+	    }
+      check = 5;
+      }
+	} else if (random == button[6]) {
+          if (check == 6) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[6].style.display = "block";
+	    }
+      check = 6;
+      }
+	} else if (random == button[7]) {
+      if (check == 7) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[7].style.display = "block";
+	    }
+      check = 7;
+      }
+	} else if (random == button[8]) {
+      if (check == 8) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[8].style.display = "block";
+	    }
+      check = 8;
+      }
+	} else if (random == button[9]) {
+      if (check == 9) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[9].style.display = "block";
+	    }
+      check = 9;
+      }
+	} else if (random == button[10]) {
+      if (check == 10) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[10].style.display = "block";
+	    }
+      check = 10;
+      }
+	} else if (random == button[11]) {
+          if (check == 11) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[11].style.display = "block";
+	    }
+      check = 11;
+      }
+	} else if (random == button[12]) {
+      if (check == 12) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[12].style.display = "block";
+	    }
+      check = 12;
+      }
+	} else if (random == button[13]) {
+      if (check == 13) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[13].style.display = "block";
+	    }
+      check = 13;
+      }
+	} else if (random == button[14]) {
+      if (check == 14) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[14].style.display = "block";
+	    }
+      check = 14;
+      }
+	} else if (random == button[15]) {
+      if (check == 15) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[15].style.display = "block";
+	    }
+      check = 15;
+      }
+	} else if (random == button[16]) {
+      if (check == 16) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[16].style.display = "block";
+	    }
+      check = 16;
+      }
+	} else if (random == button[17]) {
+      if (check == 17) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[17].style.display = "block";
+	    }
+        check = 17;
+      }
+	} else if (random == button[18]) {
+      if (check == 18) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[18].style.display = "block";
+	    }
+        check = 18;
+      }
+	} else if (random == button[19]) {
+      if (check == 19) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[19].style.display = "block";
+	    }
+        check = 19;
+      }
+	} else if (random == button[20]) {
+      if (check == 20) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[20].style.display = "block";
+	    }
+        check = 20;
+      }
+	} else if (random == button[21]) {
+      if (check == 21) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[21].style.display = "block";
+	    }
+      check = 21;
+      }
+	} else if (random == button[22]) {
+      if (check == 22) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[22].style.display = "block";
+	    }
+      check = 22;
+      }
+	} else if (random == button[23]) {
+      if (check == 23) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[23].style.display = "block";
+	    }
+      check = 23;
+      }
+	} else if (random == button[24]) {
+      if (check == 24) {
+        generateRandom2();
+        randomMove();
+      } else {
+        for (i = 0; i < button.length; i++) {
+          button[i].style.display = "none";
+          button[23].style.display = "block";
+	    }
+      check = 24;
+      }
+	}
+    } else {
+    if (random == button[0]) {
+      if (check == 0) {
+        generateRandom();
+        randomMove();
     } else {
       for (i = 0; i < button.length; i++) {
         button[i].style.display = "none";
@@ -409,7 +956,7 @@ function randomMove() {
       check = 23;
       }
 	} else if (random == button[24]) {
-      if (check == 23) {
+      if (check == 24) {
         generateRandom();
         randomMove();
       } else {
@@ -420,23 +967,38 @@ function randomMove() {
       check = 24;
       }
 	}
+  }
 }
 function reset() {
   window.location.reload();
 }
 function onClickMove() {
   if (viewportWidth < 776) {
+    console.log("mobile portrait");
     generateRandom2();
+    console.log("generate random 2");
     randomMove();
   } else if (viewportHeight < 801) {
+    console.log("mobile landscape");
     generateRandom2();
+    console.log("generate random 2");
 	randomMove();
   } else {
+    console.log("desktop");
     generateRandom();
+    console.log("generate random 2");
     randomMove();
   }
 }
-function removePoint() {
+/* function removePoint() {
   clicks -= 1;	
   score.innerHTML = "Score: "+ clicks;
+} */
+function generateRandom() {
+  random = button[Math.floor(Math.random() *button.length)];
+  console.log(random);
+}
+function generateRandom2() {
+    random = button[Math.floor(Math.random() * 16)];
+    console.log(random);
 }
